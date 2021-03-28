4b8bd412620fd5e6657e35892d09051b8a574f0b
anna-hayakawa <6.sc0wo.st9@gmail.com>
2021-03-28 16:55:49 +0900

    Laravel10.課題

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+});
+
+
+//Laravel10.課題3
+Route::get('XXX', 'AAAController@bbb');
+
+
+//Laravel10.課題4
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+});
+
