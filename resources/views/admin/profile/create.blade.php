2bd0347eefb55616ea30921836eb49730f46355e
anna-hayakawa <6.sc0wo.st9@gmail.com>
2021-04-08 20:46:06 +0900

    task_14 retry

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -38,6 +38,7 @@
                         <div class="col-md-10">
                             <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
                         </div>
+                    </div>
                     {{ csrf_field() }}
                     <input type="submit" class="btn btn-primary" value="更新">
                 </form>
